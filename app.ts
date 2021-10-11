e51fe47292c2de77b0b7313101336a42e12b3164
Aline4018 <aline12de@gmail.com>
2021-10-11 10:10:07 -0300

    Definição de cada variavel do Typescript

---
+++ app.ts
@@ -0,0 +1,13 @@
+var num: number = 10 ;
+
+var bool: boolean = true;
+
+var str: string = “ A beautiful message”;
+
+var str2: string = ‘ A beautiful message’
+
+var str3: string = `${str2} to william`;
+
+Console.log(str3);
+
+Function func() { }
